11b12f33658b663751b913a564ffc7a6a77690a6
vladobra <vladobra@SentienceLab.home>
2020-02-13 21:02:25 +0100

    ispravka teksta

--- src/com/skolarajak/App.java
+++ src/com/skolarajak/App.java
@@ -14,7 +14,7 @@ import com.skolarajak.servisi.AdministriranjeVozila;
 public class App {
 	public static void main(String[] args) {
 		Date datum = new Date();
-		System.out.println("Pocetak rada u: " + datum.toString());
+		System.out.println("Pocetak rada aplikacije: " + datum.toString());
 		AdministriranjeVozila generisiVozila = new AdministriranjeVozila();
 		List<Vozilo> vozila = generisiVozila.generisi();
 		System.out.println(vozila.size());
