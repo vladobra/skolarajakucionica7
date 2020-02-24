dd9acd0d00dc4e70fb5c08a4260dfd22154bfa72
vladobra <vladobra@SentienceLab.home>
2020-02-24 21:46:17 +0100

    Dao Layer

--- src/com/skolarajak/servisi/AdministriranjeVozila.java
+++ src/com/skolarajak/servisi/AdministriranjeVozila.java
@@ -5,8 +5,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.skolarajak.dao.VoziloInMemoryDAOImpl;
 import com.skolarajak.model.Vozilo;
 import com.skolarajak.utils.Konstante;
+import com.skolarajak.utils.RandomUtils;
 /**
  * Servis za administrativne operacije sa vozilima
  * 
@@ -16,42 +18,39 @@ import com.skolarajak.utils.Konstante;
 public class AdministriranjeVozila {
 	private static final boolean STATUS = true;
 	private static final double PRAG_RASPODELE_AKTIVNIH_VOZILA = 0.4;
-	private static final int SLOVO_A = 65;
-	private static final int SLOVO_Z = 90;
 	
-	private static final HashMap<String,Vozilo> registarskiBrojevi = new HashMap<String, Vozilo>();
+	private VoziloInMemoryDAOImpl voziloDAO;
+
+	public AdministriranjeVozila() {
+		voziloDAO = new VoziloInMemoryDAOImpl();
+	}
 	/**
 	 * Vrati test vozila
 	 * @return List<Vozilo> test vozila
 	 */
     public List<Vozilo> generisi() {
     	List<Vozilo> vozila = new ArrayList<Vozilo>();
-    	String registarskiBroj = "";
+    	Vozilo zadnjeVozilo = null;
     	for(int i = 0; i < Konstante.UKUPAN_BROJ_VOZILA_U_SISTEMU; i++) {
     		int godinaProizvodnje = dodeliGodinuProizvodnje();
     		Vozilo vozilo = new Vozilo(godinaProizvodnje);
     		vozilo.setAktivno(Math.random() > PRAG_RASPODELE_AKTIVNIH_VOZILA);
-    		registarskiBroj = kreirajRegistarskiBroj();
-    		vozilo.setRegistarskiBroj(registarskiBroj);
     		vozila.add(vozilo);
-    		registarskiBrojevi.put(registarskiBroj, vozilo);
+    		zadnjeVozilo = voziloDAO.create(vozilo);
     	}
-    	System.out.println("UKUPNO reg brojeva: " + registarskiBrojevi.keySet().size());
-    	System.out.println("UKUPNO vozila: " + registarskiBrojevi.values().size());
-    	System.out.println("Godina proizvodnje poslednjeg registrovanog: " + registarskiBroj + " : " + registarskiBrojevi.get(registarskiBroj).getGodisteProizvodnje());
+    	
+    	System.out.println("UKUPNO reg brojeva: " + voziloDAO.count());
+    	System.out.println("Godina proizvodnje poslednjeg registrovanog: " + zadnjeVozilo.getRegistarskiBroj() + " : " + voziloDAO.read(zadnjeVozilo.getRegistarskiBroj()).isAktivno());
+    	
+    	zadnjeVozilo.setAktivno(!zadnjeVozilo.isAktivno());
+    	zadnjeVozilo = voziloDAO.update(zadnjeVozilo);
+    	System.out.println("Godina proizvodnje poslednjeg registrovanog: " + zadnjeVozilo.getRegistarskiBroj() + " : " + voziloDAO.read(zadnjeVozilo.getRegistarskiBroj()).isAktivno());
+    	voziloDAO.delete(zadnjeVozilo.getRegistarskiBroj());
+    	System.out.println("Godina proizvodnje poslednjeg registrovanog: " + zadnjeVozilo.getRegistarskiBroj() + " : " + voziloDAO.read(zadnjeVozilo.getRegistarskiBroj()).isAktivno());
 		return vozila;
     }
     
     public List<Vozilo> euro3Vozila(List<Vozilo> vozila) {
-    	/*
-    	List<Vozilo> euro3Vozila = new ArrayList<Vozilo>();
-    	for (Vozilo vozilo : vozila) {
-    		if (vozilo.getGodisteProizvodnje() >= Konstante.EURO_3_GODISTE) {
-    			euro3Vozila.add(vozilo);
-    		}
-    	}
-    	*/
-    	
     	List<Vozilo> euro3Vozila = vozila.stream()
     			                   .filter(v -> v.getGodisteProizvodnje() >= 2010)
     			                   .collect(Collectors.toList());
@@ -60,14 +59,6 @@ public class AdministriranjeVozila {
     }
     
     public List<Vozilo> aktivnaVozila(List<Vozilo> vozila) {
-    	/*
-    	List<Vozilo> aktivnaVozila = new ArrayList<Vozilo>();
-    	for (Vozilo vozilo : vozila) {
-    		if (vozilo.isAktivno()) {
-    			aktivnaVozila.add(vozilo);
-    		}
-    	}
-    	*/
     	List<Vozilo> aktivnaVozila = vozila.stream()
                 .filter(v -> v.isAktivno())
                 .collect(Collectors.toList());
@@ -75,41 +66,10 @@ public class AdministriranjeVozila {
     	return aktivnaVozila;
     }
     
-    private String kreirajRegistarskiBroj() {
-    	/*String registarskiBroj = "Reg-"+slucajnoSlovo()+slucajnoSlovo();
-    	if (registarskiBrojevi.containsKey(registarskiBroj)) {
-    		System.out.println("*********** DUPLICAT **************" + registarskiBroj);
-    		return kreirajRegistarskiBroj();
-    	}
-    	registarskiBrojevi.put(registarskiBroj, registarskiBroj);
-        return registarskiBroj;*/
-    	
-    	String registarskiBroj = "";
-    	
-    	while(1==1) {
-            registarskiBroj = "Reg-"+slucajnoSlovo()+slucajnoSlovo();
-    		if (!registarskiBrojevi.containsKey(registarskiBroj)) {
-    			registarskiBrojevi.put(registarskiBroj, null);
-    			break;
-    		} else {
-    			System.out.println("*********** DUPLICAT **************" + registarskiBroj);
-    		}
-    	}
-    	return registarskiBroj;
-    }
-    
     private int dodeliGodinuProizvodnje() {
     	// godiste <-- {Godiste random 1960 - 2019}
-    	int godina  = slucajanBrojUintervalu(Konstante.MIN_VOZILO_GODISTE, Konstante.MAX_VOZILO_GODISTE);
+    	int godina  = RandomUtils.slucajanBrojUintervalu(Konstante.MIN_VOZILO_GODISTE, Konstante.MAX_VOZILO_GODISTE);
     	return godina;
     }
-    
-    private String slucajnoSlovo() {
-    	char c = (char)  slucajanBrojUintervalu(SLOVO_A, SLOVO_Z);
-    	return String.valueOf(c);
-    }
-    
-    private int slucajanBrojUintervalu(int min, int max) {
-    	return (int)(Math.random() * (max - min) + min);
-    }
+   
 }
