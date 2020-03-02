c39d32154ef3d991191fbace4b25cc4b84e5b62a
vladobra <vladobra@SentienceLab.home>
2020-03-02 20:48:10 +0100

    Vlasnik

---
+++ src/com/skolarajak/model/Vlasnik.java
@@ -0,0 +1,26 @@
+package com.skolarajak.model;
+
+public class Vlasnik {
+	private String ime;
+	private String prezime;
+	private String brojVozackeDozvole;
+	
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	public String getPrezime() {
+		return prezime;
+	}
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+	public String getBrojVozackeDozvole() {
+		return brojVozackeDozvole;
+	}
+	public void setBrojVozackeDozvole(String brojVozackeDozvole) {
+		this.brojVozackeDozvole = brojVozackeDozvole;
+	}
+}
