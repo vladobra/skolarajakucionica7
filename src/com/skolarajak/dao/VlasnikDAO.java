79850fe4dfda98cc9012608caf2150166d94b7dd
vladobra <vladobra@SentienceLab.home>
2020-04-09 20:20:11 +0200

    Komentari

--- src/com/skolarajak/dao/VlasnikDAO.java
+++ src/com/skolarajak/dao/VlasnikDAO.java
@@ -4,8 +4,18 @@ import java.util.List;
 
 import com.skolarajak.exceptions.dao.ResultNotFoundException;
 import com.skolarajak.model.Vlasnik;
-
+/**
+ * Vlasnik DAO interface
+ * 
+ * @author vladobra
+ *
+ */
 public interface VlasnikDAO {
+	/**
+	 * Kreiranje vlasnika
+	 * @param vlasnik Vlasnik
+	 * @return Kreiran vlasnik
+	 */
 	Vlasnik create(Vlasnik vlasnik);
 	Vlasnik read(String brojVozackeDozvole) throws ResultNotFoundException;
 	Vlasnik update(Vlasnik vlasnik);
